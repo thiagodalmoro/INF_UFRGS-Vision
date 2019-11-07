7bd96c4927ad9748406b367645cc2a6dfd1f7e3b
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2019-11-07 00:46:11 -0300

    refactor: divide in functions and add documentation

--- Trab2_Vision/detect_line2.py
+++ Trab2_Vision/detect_line2.py
@@ -1,64 +1,78 @@
 import cv2
 import numpy as np
 
+# TODO: check if this is the proper name for this function
 def Hough (image, list):
+    print('list param entrada:')
+    for l in list:
+        print(l)
+    print('')
+    for line in list:
+        #We want to transform polar coordinates to "normal" coordinates
+        for rho, theta in line:
+            print(line)
+            print()
+            cosTheta = np.cos(theta)
+            sinTheta = np.sin(theta)
+            x0 = cosTheta * rho
+            y0 = sinTheta * rho
 
-    for i in range(2):
-        # print(i)
-        for rho, theta in list[i]:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            x1 = int(x0 + 1000 * (-b))
-            y1 = int(y0 + 1000 * (a))
-            x2 = int(x0 - 1000 * (-b))
-            y2 = int(y0 - 1000 * (a))
+            #Calculate coordintes of the first point
+            x1 = int(x0 + 1100 * (-sinTheta)) # ( r * cos(theta) - 1100 sin(theta))
+            y1 = int(y0 + 1100 * (cosTheta))  # ( r * sin(theta) + 1100 cos(theta))
+
+            #Calculate coordintes of the second point
+            x2 = int(x0 - 1100 * (-sinTheta)) # ( r * cos(theta) + 1100 sin(theta))
+            y2 = int(y0 - 1100 * (cosTheta))  # ( r * sin(theta) - 1100 cos(theta))
 
             cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
 
-img1 = cv2.imread('exemplo1.jpg')
-gray = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,50,150,apertureSize = 3)
-lines = cv2.HoughLines(edges, 1, np.pi / 180, 150, None, 0, 0)
-print(lines)
 
-ex1 = []
-ex1.append(lines[0])
-ex1.append(lines[2])
+def find_and_draw_axis(src_image):
+    editing_image = src_image
 
-Hough(img1,ex1)
+    # Convert into gray scale image (for canny edge detection is preferred gray scale images)
+    gray = cv2.cvtColor(editing_image,cv2.COLOR_BGR2GRAY)
 
-img2 = cv2.imread('exemplo2.jpg')
-gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,50,150,apertureSize = 3)
-lines = cv2.HoughLines(edges, 1, np.pi / 180, 150, None, 0, 0)
-print(lines)
+    # Process Edge Detection (gets the canny edge detection image)
+    lower_threshold = 50
+    higher_threshold = 150
+    edges = cv2.Canny(gray, lower_threshold, higher_threshold, apertureSize = 3)
 
-ex2 = []
-ex2.append(lines[0])
-ex2.append(lines[2])
+    #Apply Hough Transform (that returns the vector of lines) into the canny edge detection image
+    accumulator_threshold = 180 # Threshold to consider a line
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, accumulator_threshold, None, 0, 0)
+    print(lines)
 
-Hough(img2,ex2)
+    # TODO: check how to find 2 proper lines
+    ex1 = lines[0::1]
+    # ex1.append(lines[0])
+    # ex1.append(lines[2])
 
-img3 = cv2.imread('exemplo3.jpg')
-gray = cv2.cvtColor(img3,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,50,150,apertureSize = 3)
-lines = cv2.HoughLines(edges, 1, np.pi / 180, 150, None, 0, 0)
-print(lines)
+    Hough(editing_image,lines)
 
-ex3 = []
-ex3.append(lines[3])
-ex3.append(lines[2])
+    return editing_image
 
-Hough(img3,ex3)
 
-cv2.imshow('Image1', img1)
-cv2.imshow('Image2', img2)
-cv2.imshow('Image3', img3)
 
-key = cv2.waitKey(0)
-if key == 27:  # wait for ESC
-    print('Key ESC pressed.')
-    cv2.destroyAllWindows()
+# main()
+if __name__ == '__main__' :
+
+    img1 = cv2.imread('exemplo1.jpg')
+    img1 = find_and_draw_axis(img1)
+    cv2.imshow('Image1', img1)
+
+    img2 = cv2.imread('exemplo2.jpg')
+    img2 = find_and_draw_axis(img2)
+    cv2.imshow('Image2', img2)
+
+    img3 = cv2.imread('exemplo3.jpg')
+    img3 = find_and_draw_axis(img3)
+    cv2.imshow('Image3', img3)
+    # cv2.destroyAllWindows()
+
+    key = cv2.waitKey(0)
+    if key == 27:  # wait for ESC
+        print('Key ESC pressed.')
+        cv2.destroyAllWindows()
