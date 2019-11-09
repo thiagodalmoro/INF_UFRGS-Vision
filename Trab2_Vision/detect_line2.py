ea9d0dbb674efbf431a83ecbde37db690b4d1aa4
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2019-11-09 14:06:45 -0300

    refactor: functions for image processing and lines detection

--- Trab2_Vision/detect_line2.py
+++ Trab2_Vision/detect_line2.py
@@ -1,75 +1,79 @@
 import cv2
 import numpy as np
 
-# TODO: check if this is the proper name for this function
-def Hough (image, list):
-    print('list param entrada:')
-    for l in list:
-        print(l)
-    print('')
-    for line in list:
+def translate_polar_coordinates (src_image, hough_lines):
+    editing_image = src_image
+    # Calulate the image's larger measure (lenght or width) to fit the lines we will draw in the image
+    img_max_size = max(src_image.shape) * 1.1
+
+    lines_list = []
+    # We iterate over the hough_lines
+    for line in hough_lines[0:-1]:
         #We want to transform polar coordinates to "normal" coordinates
         for rho, theta in line:
-            print(line)
-            print()
             cosTheta = np.cos(theta)
             sinTheta = np.sin(theta)
             x0 = cosTheta * rho
             y0 = sinTheta * rho
 
-            #Calculate coordintes of the first point
-            x1 = int(x0 + 1100 * (-sinTheta)) # ( r * cos(theta) - 1100 sin(theta))
-            y1 = int(y0 + 1100 * (cosTheta))  # ( r * sin(theta) + 1100 cos(theta))
+            #Calculate coordinates of the first point: r * cos(theta) - img_max_size * [sin(theta) or cos(theta)]
+            x1 = int(x0 + img_max_size * (-sinTheta)) # ( r * cos(theta) - img_max_size * sin(theta))
+            y1 = int(y0 + img_max_size * (cosTheta))  # ( r * sin(theta) + img_max_size * cos(theta))
 
-            #Calculate coordintes of the second point
-            x2 = int(x0 - 1100 * (-sinTheta)) # ( r * cos(theta) + 1100 sin(theta))
-            y2 = int(y0 - 1100 * (cosTheta))  # ( r * sin(theta) - 1100 cos(theta))
+            #Calculate coordinates of the second point
+            x2 = int(x0 - img_max_size * (-sinTheta)) # ( r * cos(theta) + img_max_size * sin(theta))
+            y2 = int(y0 - img_max_size * (cosTheta))  # ( r * sin(theta) - img_max_size * cos(theta))
+            coordinates = {'x1':x1,'x2':x2,'y1':y1,'y2':y2}
+            lines_list.append(coordinates)
 
-            cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    return lines_list
 
-
-
-def find_and_draw_axis(src_image):
-    editing_image = src_image
-
+def process_image(image):
     # Convert into gray scale image (for canny edge detection is preferred gray scale images)
-    gray = cv2.cvtColor(editing_image,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
     # Process Edge Detection (gets the canny edge detection image)
-    lower_threshold = 50
-    higher_threshold = 150
+    lower_threshold = 100
+    higher_threshold = 200
     edges = cv2.Canny(gray, lower_threshold, higher_threshold, apertureSize = 3)
 
+    return edges
+
+def find_lines(image):
+
     #Apply Hough Transform (that returns the vector of lines) into the canny edge detection image
-    accumulator_threshold = 180 # Threshold to consider a line
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, accumulator_threshold, None, 0, 0)
-    print(lines)
+    accumulator_threshold = 150 # Threshold to consider a line ("votes necessaries to be considered line")
+    hough_lines = cv2.HoughLines(image, 1, np.pi / 180, accumulator_threshold)
 
-    # TODO: check how to find 2 proper lines
-    ex1 = lines[0::1]
-    # ex1.append(lines[0])
-    # ex1.append(lines[2])
+    lines = translate_polar_coordinates(image, hough_lines)
 
-    Hough(editing_image,lines)
+    return lines
 
-    return editing_image
+def draw_lines(src_image, lines):
+    for line in lines:
+        cv2.line(src_image, ( line['x1'],  line['y1']), ( line['x2'],  line['y2']), (0, 0, 255), 2)
 
+def find_and_draw_axis(src_image):
+    processed_image = process_image(src_image)
+    lines = find_lines(processed_image)
+    draw_lines(src_image, lines)
 
+    return src_image
 
 # main()
 if __name__ == '__main__' :
 
-    img1 = cv2.imread('exemplo1.jpg')
+    img1 = cv2.imread('t.jpg')
     img1 = find_and_draw_axis(img1)
     cv2.imshow('Image1', img1)
 
-    img2 = cv2.imread('exemplo2.jpg')
-    img2 = find_and_draw_axis(img2)
-    cv2.imshow('Image2', img2)
-
-    img3 = cv2.imread('exemplo3.jpg')
-    img3 = find_and_draw_axis(img3)
-    cv2.imshow('Image3', img3)
+    # img2 = cv2.imread('exemplo2.jpg')
+    # img2 = find_and_draw_axis(img2)
+    # cv2.imshow('Image2', img2)
+    #
+    # img3 = cv2.imread('exemplo3.jpg')
+    # img3 = find_and_draw_axis(img3)
+    # cv2.imshow('Image3', img3)
     # cv2.destroyAllWindows()
 
     key = cv2.waitKey(0)
